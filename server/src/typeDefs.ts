ff7aaf231b3dbf27388377bd742a1f12ba24cfb3
David <dhahn123@gmail.com>
2019-05-07 17:27:17 -0700

    Bcrypt: hashing passwords, setting up Mutation types, index.ts:startServer();

--- server/src/typeDefs.ts
+++ server/src/typeDefs.ts
@@ -3,5 +3,8 @@ import {gql} from "apollo-server-express";
 export const typeDefs = gql`
     type Query {
         hello: String!
+    },
+    type Mutation {
+        register(email: String!, password: String!): Boolean!
     }
 `;
