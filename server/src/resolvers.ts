9954d53bb3afcce4aa5bf8fda932f63f17778222
David <dhahn123@gmail.com>
2019-05-09 17:56:20 -0700

    Set up test Stripe configuration for customers, Mutation: createSubscription, entity/User.ts: @Column for stripeId, type

--- server/src/resolvers.ts
+++ server/src/resolvers.ts
@@ -1,7 +1,7 @@
-import { IResolvers } from "graphql-tools";
 import * as bcrypt from "bcryptjs";
-
-import {User} from './entity/User';
+import { IResolvers } from "graphql-tools";
+import { User } from './entity/User';
+import { stripe } from "./stripe";
 
 export const resolvers: IResolvers= {
     Query: {
@@ -10,7 +10,7 @@ export const resolvers: IResolvers= {
             if(!req.session.userId) {
                 return null;
             }
-            
+
             return User.findOne(req.session.userId);
         }
     },
@@ -37,6 +37,31 @@ export const resolvers: IResolvers= {
 
         req.session.userId = user.id;
         return user;
+        },
+        // source represents credit card
+        createSubscription: async (_, {source}, {req}) => {
+            // checks if users have user id or session available
+            if(!req.session || !req.session.userId) {
+                throw new Error("Not Authenticated")
+            }
+
+            const user = await User.findOne(req.session.userId);
+
+            if (!user) {
+                throw new Error();
+            }
+
+            const customer = await stripe.customers.create({
+                email: user.email,
+                source,
+                plan: process.env.PLAN
+            });
+
+            user.stripeId = customer.id;
+            user.type = "test";
+            await user.save();
+
+            return user;
         }
     }
 }
