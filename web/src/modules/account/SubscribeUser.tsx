0df839b8493a7c9f6b33aa6639fd76768a87af37
David <dhahn123@gmail.com>
2019-05-09 19:07:15 -0700

    Create SubscribeUser.tsx: Subscription Charging now works, Authentication is req., Stripe now tracks subscribers

--- web/src/modules/account/SubscribeUser.tsx
+++ web/src/modules/account/SubscribeUser.tsx
@@ -1,15 +1,35 @@
-import * as React from 'react';
-import StripeCheckout from 'react-stripe-checkout';
+import { gql } from "apollo-boost";
+import * as React from "react";
+import { Mutation } from "react-apollo";
+import StripeCheckout from "react-stripe-checkout";
+import { CreateSubscriptionMutation, CreateSubscriptionMutationVariables } from '../../schemaTypes';
+
+const createSubscriptionMutation = gql`
+  mutation CreateSubscriptionMutation($source: String!) {
+    createSubscription(source: $source) {
+      id
+      email
+    }
+  }
+`;
 
 export default class SubscribeUser extends React.PureComponent {
   render() {
     return (
-      <StripeCheckout
-        token={(token) => {
-            console.log(token)
-        }}
-        stripeKey={process.env.REACT_APP_STRIPE_PUBLISHABLE!}
-      />
-    )
+      <Mutation<CreateSubscriptionMutation, CreateSubscriptionMutationVariables>
+      mutation={createSubscriptionMutation}
+        >
+        {(mutate) => (
+          <StripeCheckout
+            token={async token => {
+              const response = await mutate({variables: {source: token.id}
+              });
+              console.log(response);
+            }}
+            stripeKey={process.env.REACT_APP_STRIPE_PUBLISHABLE!}
+          />
+        )}
+      </Mutation>
+    );
   }
 }
