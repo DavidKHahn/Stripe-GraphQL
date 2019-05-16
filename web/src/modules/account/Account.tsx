6e7f90c1b659753d1e25d77607e603785c1006a0
David <dhahn123@gmail.com>
2019-05-16 11:56:38 -0700

    auto mutate: type in Subscribe.tsx, return 'Thank you...' once paid

--- web/src/modules/account/Account.tsx
+++ web/src/modules/account/Account.tsx
@@ -2,8 +2,9 @@ import { gql } from "apollo-boost";
 import * as React from "react";
 import { PureComponent } from "react";
 import { Query } from "react-apollo";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import { MeQuery } from "../../schemaTypes";
+import SubscribeUser from "./SubscribeUser";
 
 const meQuery = gql`
   query MeQuery {
@@ -18,7 +19,7 @@ const meQuery = gql`
 export default class Account extends PureComponent {
   render() {
     return (
-    // fetchPolicy ignore caching and goes directly to server each time
+      // fetchPolicy ignore caching and goes directly to server each time
       <Query<MeQuery> fetchPolicy="network-only" query={meQuery}>
         {({ data, loading }) => {
           if (loading) {
@@ -32,7 +33,12 @@ export default class Account extends PureComponent {
             return <Link to="/login">Please Login</Link>;
           }
 
-          return <div>{data.me.type}</div>;
+          if (data.me.type === "free-trial") {
+            return <SubscribeUser />;
+          }
+
+          //   if (data.me.type === 'paid')
+          return <div>Thank you and come again!</div>;
         }}
       </Query>
     );
